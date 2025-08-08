eebe499aa9111c58f6d3795ecb2928284ccae5fe
jhon oliveira <joheneydson@gmail.com>
2025-08-07 21:41:46 -0300

    exercicio da aula 02 resolvido com sucesso

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,25 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+app = FastAPI(title="Minha API foda.")
+
+status_200_do_HTTPStatus = HTTPStatus.OK
 
 
-@app.get('/')
-def read_root():
-    return {'message': 'Ola mundo'}
+@app.get(
+    "/ola_mundo",
+    status_code=status_200_do_HTTPStatus,
+    response_class=HTMLResponse,
+)
+def retorna_html():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,28 @@
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from fastapi_zero.app import app
 
 
-def test_root_deve_retornar_ola_mundo():
-    client = TestClient(app)
+def test_restorna_html_deve_retornar_html():
 
-    response = client.get("/")
-    assert response.json() == {"message": "Ola mundo"}
+    client = TestClient(app)
+    response = client.get("/ola_mundo")
+
+    status_da_response_e_OK = response.status_code == HTTPStatus.OK
+
+    html_para_test = """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
+
+    html_da_response_e_igual_a_html_para_test = response.text == html_para_test
+
+    assert status_da_response_e_OK
+    assert html_da_response_e_igual_a_html_para_test
