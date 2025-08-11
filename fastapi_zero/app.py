929438c1dfb31ea25defbda34eb12d2fa4866b94
jhon oliveira <joheneydson@gmail.com>
2025-08-11 19:15:14 -0300

    Implementando rotas CRUD

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -42,6 +42,13 @@ def update_user(user_id: int, user: UserSchema):
 @app.delete('/users/{user_id}', status_code=status_200_ok, response_model=UserPublic)
 def delete_user(user_id: int):
     if user_id > len(database) or user_id < 1:
-        raise HTTPException(status_code=status_404_NOT_FOUND, detail='user not found')
+        raise HTTPException(status_code=status_404_NOT_FOUND, detail='User not found')
 
     return database.pop(user_id - 1)
+
+
+@app.get('/users/{user_id}', response_model=UserPublic)
+def read_user__exercicio(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='User not found')
+    return database[user_id - 1]
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,10 @@
+from http import HTTPStatus
+
+
 def test_create_user(client, codigo_201_dado_enviado_e_criado_com_sucesso):
     response = client.post(
-        '/users/',
-        json={'username': 'jhon', 'email': 'jhon@gmail.com', 'password': '123'},
+        "/users/",
+        json={"username": "bob", "email": "bob@example.com", "password": "123"},
     )
 
     json_da_response = response.json()
@@ -9,52 +12,61 @@ def test_create_user(client, codigo_201_dado_enviado_e_criado_com_sucesso):
     status_code_da_response = response.status_code
 
     assert status_code_da_response == codigo_201_dado_enviado_e_criado_com_sucesso
-    assert json_da_response == {'username': 'jhon', 'email': 'jhon@gmail.com', 'id': 1}
+    assert json_da_response == {"username": "bob", "email": "bob@example.com", "id": 1}
 
 
 def test_read_users(client, codigo_200_dado_solicitado_e_retornado_com_sucesso):
-    response = client.get('/users/')
+    response = client.get("/users/")
 
     status_code_da_response = response.status_code
     json_da_response = response.json()
 
     assert status_code_da_response == codigo_200_dado_solicitado_e_retornado_com_sucesso
     assert json_da_response == {
-        'users': [{'username': 'jhon', 'email': 'jhon@gmail.com', 'id': 1}]
+        "users": [{"username": "bob", "email": "bob@example.com", "id": 1}]
     }
 
 
 def test_update_user(client, codigo_200_dado_atualizado_com_sucesso):
     response = client.put(
-        '/users/1',
+        "/users/666",
         json={
-            'username': 'bob',
-            'email': 'bob@example.com',
-            'password': 'mynewpassword',
+            "username": "bob",
+            "email": "bob@example.com",
+            "password": "mynewpassword",
         },
     )
 
     status_code_da_response = response.status_code
     json_da_response = response.json()
 
-    assert status_code_da_response == codigo_200_dado_atualizado_com_sucesso
-    assert json_da_response == {
-        'username': 'bob',
-        'email': 'bob@example.com',
-        'id': 1,
-    }
+    assert status_code_da_response == HTTPStatus.NOT_FOUND
+    assert json_da_response == {"detail": "User not found"}
 
 
 def test_delete_user(client, codigo_200_dado_excluido_com_sucesso):
-    response = client.delete('/users/1')
+    response = client.delete("/users/111")
 
     status_code_da_response = response.status_code
     json_da_response = response.json()
 
-    assert json_da_response == {
-        'username': 'bob',
-        'email': 'bob@example.com',
-        'id': 1,
+    assert status_code_da_response == HTTPStatus.NOT_FOUND
+    assert json_da_response == {"detail": "User not found"}
+
+
+def test_get_user_should_return_not_found__exercicio(client):
+    response = client.get("/users/666")
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {"detail": "User not found"}
+
+
+def test_get_user__exercicio(client):
+    response = client.get("/users/1")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "username": "bob",
+        "email": "bob@example.com",
+        "id": 1,
     }
-    assert status_code_da_response == codigo_200_dado_excluido_com_sucesso
-    
